2af0057b0c0f90f5a97523561c02578f0db0447f
Nikolay1992167 <wulik1081@mail.ru>
2023-12-05 15:30:58 +0300

    test: adding ClientTest and ServerTest

--- src/test/java/by/clevertec/entity/ClientTest.java
+++ src/test/java/by/clevertec/entity/ClientTest.java
@@ -1,32 +1,119 @@
 package by.clevertec.entity;
 
+import by.clevertec.exception.ClientException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class ClientTest {
 
-    @Test
-    void sendRequest() {
+    private static final int DATA_SIZE = 10;
+    private static final int NUMBER_OF_THREADS = 4;
+    private Client client;
+
+    @BeforeEach
+    void setup() {
+        client = new Client(DATA_SIZE, NUMBER_OF_THREADS);
     }
 
     @Test
-    void getData() {
+    void shouldThreadReturnNotZeroValue() throws ExecutionException, InterruptedException {
+        // given
+        Future<Client.Request> future = client.getExecutor().submit(client.new Request(1));
+
+        // when
+        int actual = future.get().getDataRequest();
+
+        // then
+        assertThat(actual).isNotZero();
     }
 
     @Test
-    void getExecutor() {
+    void shouldThreadReturnValidValue() throws ExecutionException, InterruptedException {
+        // given
+        Future<Client.Request> future = client.getExecutor().submit(client.new Request(1));
+
+        // when
+        int validValue = future.get().getDataRequest();
+
+        // then
+        assertThat(validValue >= 1 && validValue <= DATA_SIZE).isTrue();
     }
 
     @Test
-    void getLock() {
+    void shouldThreadRemoveDifferentValuesFromDataList() throws ExecutionException, InterruptedException {
+        // given
+        List<Future<Client.Request>> futures = List.of(
+                client.getExecutor().submit(client.new Request(1)),
+                client.getExecutor().submit(client.new Request(2))
+        );
+
+        // when
+        int result1 = futures.get(0).get().getDataRequest();
+        int result2 = futures.get(1).get().getDataRequest();
+
+        // then
+        assertThat(result1).isNotEqualTo(result2);
     }
 
     @Test
-    void getAccumulator() {
+    void shouldReducedDataListSizeByRequestThread() {
+        // given
+        List<Future<Client.Request>> futures = List.of(
+                client.getExecutor().submit(client.new Request(1)),
+                client.getExecutor().submit(client.new Request(2))
+        );
+
+        // when
+        futures.forEach(future -> client.getServer().processRequest(future));
+
+        // then
+        assertThat(client.getData()).hasSize(DATA_SIZE - 2);
     }
 
     @Test
-    void getServer() {
+    void shouldCheckThatRemovedValuesFromDataByThread() throws ExecutionException, InterruptedException {
+        // given
+        List<Future<Client.Request>> futures = List.of(
+                client.getExecutor().submit(client.new Request(1)),
+                client.getExecutor().submit(client.new Request(2))
+        );
+
+        // when
+        int valueFirst = futures.get(0).get().getDataRequest();
+        int valueSecond = futures.get(1).get().getDataRequest();
+
+        // then
+        assertThat(client.getData()).doesNotContain(valueFirst, valueSecond);
+    }
+
+    @Test
+    void shouldTreadNotThrowAnyExceptionAndCheckForChangesListOfDataByThreadSendRequest() {
+        // when, then
+        assertThatCode(() -> client.sendRequest())
+                .doesNotThrowAnyException();
+        assertThat(client.getServer().getResourceList())
+                .isNotNull()
+                .isNotEmpty();
+    }
+
+    @Test
+    void shouldCheckAccumulatorByThreadSendRequest() {
+        // when, then
+        assertThat(client.getAccumulator().get())
+                .isNotNull()
+                .isGreaterThanOrEqualTo(0);
+    }
+
+    @Test
+    void shouldThrowsExceptionBySendRequest() {
+
     }
 }
